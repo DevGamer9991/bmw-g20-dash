7f6f4f1f6dac36aadb2aea1b2ee158487084bcb3
Parker Thornton <pathornton114@gmail.com>
2026-08-28 20:07:19 -0500

    Added Boost Pressure, IAT

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# autostart filename
+nano ~/.config/wayfire.ini
+
+# remove the mouse cursor after 0 seconds of inactivity
+unclutter -idle 0
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ SRC_ADDR = 0xF4
 DST_ADDR = 0x12
 
 app = Flask(__name__)
-# async_mode='threading' keeps it compatible with your standard socket logic
+
 socketio = SocketIO(app, async_mode='threading', cors_allowed_origins="*")
 
 def send_hsfz_command(sock, uds_payload):
@@ -47,18 +47,30 @@ def read_hsfz_response(sock, timeout=1.0):
     return b""
 
 def car_polling_thread():
-    """Background thread that connects to ENET and streams data."""
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         print(f"[*] Connecting to {AGENT_IP}:{AGENT_PORT}...")
         sock.connect((AGENT_IP, AGENT_PORT))
         print("[+] Connected!")
 
+        # Clear previous
         send_hsfz_command(sock, bytes.fromhex("2C 03 F3 00"))
         read_hsfz_response(sock)
 
-        map_payload = bytes.fromhex("2C 01 F3 00 48 07 01 02")
-        send_hsfz_command(sock, map_payload)
+        # RPM 
+        send_hsfz_command(sock, bytes.fromhex("2C 01 F3 00 48 07 01 02"))
+        read_hsfz_response(sock)
+
+        # Ambient Pressure 
+        send_hsfz_command(sock, bytes.fromhex("2C 01 F3 00 42 01 01 02"))
+        read_hsfz_response(sock)
+        
+        # Charged Air Pressure
+        send_hsfz_command(sock, bytes.fromhex("2C 01 F3 00 42 05 01 02"))
+        read_hsfz_response(sock)
+        
+        # Intake Air Temperature
+        send_hsfz_command(sock, bytes.fromhex("2C 01 F3 00 58 1E 01 01"))
         read_hsfz_response(sock)
 
         read_payload = bytes.fromhex("22 F3 00")
@@ -66,22 +78,37 @@ def car_polling_thread():
         while True:
             send_hsfz_command(sock, read_payload)
             uds_resp = read_hsfz_response(sock)
-            
-            if uds_resp.startswith(b"b\xf3\x00"):
-                data = uds_resp[3:5]
-                raw_rpm = struct.unpack(">H", data)[0]
-                rpm = raw_rpm * 0.5
-                
-                print(rpm)
-                
-                # Push data to the webpage
-                socketio.emit('rpm_data', {'rpm': rpm})
 
-            time.sleep(0.05)
+            if uds_resp.startswith(b"b\xf3\x00"):
+                rpm_raw = struct.unpack(">H", uds_resp[3:5])[0]
+                rpm = rpm_raw * 0.5
+                
+                ambient_pressure_raw = struct.unpack(">H", uds_resp[5:7])[0]
+                ambient_pressure_mbar = ambient_pressure_raw * 0.0390625
+                
+                charged_pressure_raw = struct.unpack(">H", uds_resp[7:9])[0]
+                charged_pressure_mbar = charged_pressure_raw * 0.078125
+                
+                boost_pressure_mbar = charged_pressure_mbar - ambient_pressure_mbar
+                
+                boost_psi_raw = boost_pressure_mbar / 68.94757
+                
+                boost_psi = round(boost_psi_raw, 1)
+                
+                intake_air_temp_raw = uds_resp[9]
+                intake_air_temp_c = (intake_air_temp_raw * 0.75) - 48
+                
+                intake_air_temp_f = (intake_air_temp_c * 9/5) + 32
+                
+                socketio.emit('car_data', {'rpm': rpm, 'boost_pressure': boost_psi, 'intake_air_temp': round(intake_air_temp_f)})
+
+            elif uds_resp.startswith(b"\x7F"):
+                print(f"[-] ECU busy/NACK: {uds_resp.hex()}")
+
+            time.sleep(0.1)
             
     except Exception as e:
         print(f"[!] Error: {e}")
-        # Try to reconnect after a delay if connection drops
         time.sleep(5)
         car_polling_thread()
     finally:
@@ -99,8 +126,11 @@ def download_gauge():
 def download_socket_io():
     return send_from_directory(directory="./templates", path="socket.io.js")
 
+@app.route("/fonts/<path:filename>")
+def download_font(filename):
+    return send_from_directory(directory="./templates/static/", path=filename)
+
 if __name__ == "__main__":
-    # Start the ENET polling in the background
     threading.Thread(target=car_polling_thread, daemon=True).start()
-    # Run the web server
+
     socketio.run(app, host="0.0.0.0", port=8080, allow_unsafe_werkzeug=True)
